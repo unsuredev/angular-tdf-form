207b3bd1869a941543b197afa86ccb890c5c371f
Jamaluddin Mondal <42401032+Rahul626@users.noreply.github.com>
2019-07-31 15:08:56 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { User } from "./user";
+
+import { EnrollmentService } from "./enrollment.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'datastore';
+  topics = ["Angular", "React", "Vue"];
+  userModel = new User(
+    "Rob",
+    "rob@test.com",
+    5556665566,
+    "default",
+    "morning",
+    true
+  );
+  topicHasError = true;
+  submitted = false;
+  errorMsg = "";
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+  validateTopic(value) {
+    if (value === "default") {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    // this._enrollmentService
+    //   .enroll(this.userModel)
+    //   .subscribe(
+    //     response => console.log("Success!", response),
+    //     error => (this.errorMsg = error.statusText)
+    //   );
+    console.log(this.userModel);
+  }
 }
